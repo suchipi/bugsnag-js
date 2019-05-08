cdebd7bfb597fca1f40db68ce1ba271ab7055577
Lily Scott <me@suchipi.com>
2019-05-08 17:44:39 -0600

    Include deps in babelified bundle
    I couldn't figure out the correct order for babel/resolve so I just did it as a separate step. Yolo.

--- packages/plugin-window-unhandled-rejection/rollup.config.js
+++ packages/plugin-window-unhandled-rejection/rollup.config.js
@@ -1,17 +1,9 @@
 import commonjs from "rollup-plugin-commonjs";
 import nodeResolve from "rollup-plugin-node-resolve";
 import json from "rollup-plugin-json";
-import babel from "rollup-plugin-babel";
 
 export default {
-  plugins: [
-    babel({
-      include: "**/*"
-    }),
-    nodeResolve(),
-    commonjs(),
-    json()
-  ],
+  plugins: [nodeResolve(), commonjs(), json()],
   output: {
     format: "cjs"
   }
