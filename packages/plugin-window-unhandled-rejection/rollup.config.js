d8f9c979fac41219dfbbab10e49c350a94ecd410
Lily Scott <me@suchipi.com>
2019-05-08 16:14:27 -0600

    Bundle plugin-window-unhandled-rejection so it can be consumed as ES5

---
+++ packages/plugin-window-unhandled-rejection/rollup.config.js
@@ -0,0 +1,10 @@
+import commonjs from "rollup-plugin-commonjs";
+import nodeResolve from "rollup-plugin-node-resolve";
+import json from "rollup-plugin-json";
+
+export default {
+  plugins: [nodeResolve(), commonjs(), json()],
+  output: {
+    format: "cjs"
+  }
+};
